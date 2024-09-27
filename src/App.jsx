95248dda8b5148b49db4db42dd6c4b7186508d7f
guillermoleonq <guillermoleonq@yahoo.com>
2024-09-27 06:57:02 -0500

    Carrusel Building

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { ApiCoffeeCarruselBasic } from './componets/ApiCoffeeCarruselBasic'
+
+function App() {
+
+
+  return (
+    <div>
+      <ApiCoffeeCarruselBasic />
+    </div>
+  )
+}
+
+export default App
